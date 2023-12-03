e0e5f2a346f0995ffad641019b8de7bd2e4320e0
Frank Schoeneman <frank.schoeneman@gmail.com>
2023-12-03 17:16:06 -0500

    day03 rust soln, find symbols

--- rust/03/main01.rs
+++ rust/03/main01.rs
@@ -16,12 +16,38 @@ fn input_to_grid(file: File) -> Vec<Vec<char>> {
     grid
 }
 
+fn is_symbol(this_char: char) -> bool {
+
+    // "!@#$%^&*()-+?_=,<>/"
+    let symbols = String::from("!@#$%^&*()-+?_=,<>/");
+    symbols.contains(this_char)    
+}
+
+fn find_symbols(grid: &Vec<Vec<char>>) -> Vec<char> {
+
+    let mut symbol_list = Vec::new();
+    
+    for row in grid {
+        for &this_char in row {
+
+            if is_symbol(this_char) {
+               symbol_list.push(this_char);
+            }
+        } 
+    }
+    symbol_list
+}
+
+
 fn main() -> std::io::Result<()> {
     
     let file = File::open("input.txt")?;
     
     let grid: Vec<Vec<char>> = input_to_grid(file);
+    //println!("{:?}", grid);
 
-    println!("{:?}", grid);
+    let sym: Vec<char> = find_symbols(&grid);
+    println!("{:?}", sym);
+
     Ok(())
 }
